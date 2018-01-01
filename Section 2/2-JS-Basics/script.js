51d054196863f49a840a8d430859de9e10f265fd
Jay Stringer <jay.stringer@music-group.com>
2018-01-01 20:25:27 +0000

    Section 2
    Started following along with section 2. Finished the first 2 lectures in this section.

---
+++ Section 2/2-JS-Basics/script.js
@@ -0,0 +1,40 @@
+// Lecture: Variables
+/*
+var name = "John";
+console.log(name);
+
+var last_name = "Smith";
+console.log(last_name)
+
+var age = 26;
+console.log(age);
+
+var full_age = true;
+console.log(full_age);
+*/
+
+// Lecture: Variables 2
+var name = "Jay";
+var age = 23;
+
+// console.log(name + age);
+// console.log(age + age);
+
+var job, is_married;
+
+// console.log(job);
+
+job = "Auto Tester";
+is_married = false;
+
+console.log(name + " is a " + age + " years old " + job + ". Is he married? " + is_married + ".");
+
+age = "twenty three";
+job = "developer";
+
+console.log(name + " is a " + age + " years old " + job + ". Is he married? " + is_married + ".");
+
+var last_name = prompt("Please enter your last name: ");
+console.log(last_name);
+
+alert(name + " is a " + age + " years old " + job + ". Is he married? " + is_married + ".");
